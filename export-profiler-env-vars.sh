439bc4c0a1f11d4b8826923fbab5df8a64309f27
Robert Pajak <pellared@hotmail.com>
2021-03-08 13:09:37 +0000

    Update path

--- export-profiler-env-vars.sh
+++ export-profiler-env-vars.sh
@@ -2,8 +2,8 @@
 
 export CORECLR_ENABLE_PROFILING="1"
 export CORECLR_PROFILER="{918728DD-259F-4A6A-AC2B-B85E1B658318}"
-export CORECLR_PROFILER_PATH="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Debug/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so"
-export OTEL_DOTNET_TRACER_HOME="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Debug/x64"
+export CORECLR_PROFILER_PATH="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so"
+export OTEL_DOTNET_TRACER_HOME="${PWD}/src/Datadog.Trace.ClrProfiler.Native/bin/Release/x64"
 export OTEL_INTEGRATIONS="${PWD}/integrations.json"
 export OTEL_VERSION="1.0.0"
 export OTEL_TRACE_AGENT_URL="http://localhost:9411/api/v2/spans"
